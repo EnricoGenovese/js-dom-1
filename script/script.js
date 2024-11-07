94a727f2d3faaad641fb36f06ad8ae0188c5389c
EnricoGenovese <enricogeno@libero.it>
2024-11-07 15:26:39 +0100

    add change light at button press;

--- script/script.js
+++ script/script.js
@@ -7,3 +7,21 @@ console.clear();
 // accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi 
 
+/* ----------------------------------------------------------------------------- */
+
+// Dichiarare le variabili degli elementi che servono per cambiare 
+// dinamincamente la pagina.
+
+const on = document.getElementById("on");
+const off = document.getElementById("off");
+const btn = document.getElementById("light-switch");
+
+
+function changeLight() {
+    off.classList.toggle
+}
+
+btn.addEventListener("click", function() {
+    off.src = "./img/yellow_lamp.png";
+    off.alt = "on";
+})
