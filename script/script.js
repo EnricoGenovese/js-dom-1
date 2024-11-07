4dcafd56c01c82fcc5e86e734c4c909cf0baa585
EnricoGenovese <enricogeno@libero.it>
2024-11-07 14:36:36 +0100

    init

---
+++ script/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.clear();
+
+// Consegna:
+// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato
+// accanto un bottone con la scritta “Accendi”.
+// Al click del bottone, la lampadina dovrà accendersi 
+
